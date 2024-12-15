8e40e9129f9422a121057b358da149ac7fdec018
Sfi <sfi0220@protonmail.com>
2024-12-15 14:18:45 +0000

    learning last part of part 2 part3 and starting part2exercise3

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
-
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -10,17 +10,56 @@ const App = () => {
 
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
+    noteService
+      .getAll()
+      .then(initialNotes => { setNotes(initialNotes) })
   }, [])
-  console.log('render', notes.length, 'notes')
 
-  // ...
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => { setNotes(notes.map(note => note.id === id ? returnedNote : note)) })
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))        
+        setNewNote('')
+      })
+  }
+  const handleNoteChange = (e) =>{
+    setNewNote(e.target.value)
+  }
+  const notesToShow = notes.filter(value =>{
+    return value.important === true
+  })
+  return (
+    <div>
+      <form>
+        note<input type='text' onChange={handleNoteChange}></input>
+        <button type='submit' onClick={addNote}>submit</button>
+      </form>
+      {notesToShow.map(note => {
+        return <Note
+          key={note.id}
+          note={note}
+          toggleImportance={() => toggleImportanceOf(note.id)}
+        />
+
+      })}
+    </div>
+  )
 }
 
 export default App 
